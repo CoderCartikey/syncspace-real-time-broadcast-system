8d4c0ee6d31b19e5b5e2f8ff9e918cc61891ac2a
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-09-20 14:09:56 +0530

    Update DatabaseConnection.java

--- src/com/syncspace/database/DatabaseConnection.java
+++ src/com/syncspace/database/DatabaseConnection.java
@@ -1,4 +1,16 @@
 package com.syncspace.database;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/syncspace_db";
+    private static final String USER = "root";
+
+    private DatabaseConnection() {
+    }
+
 }
+
