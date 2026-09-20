95274cdc670f777ac2f5089e9b55486c2bb284e7
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-09-20 14:26:23 +0530

    Update DatabaseConnectionTest.java

--- src/com/syncspace/database/DatabaseConnectionTest.java
+++ src/com/syncspace/database/DatabaseConnectionTest.java
@@ -1,5 +1,13 @@
 package com.syncspace.database;
-import 
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DatabaseConnectionTest {
+    public static void main(String[] args) {
+        try(Connection connection = DatabaseConnection.getConnection()){
+            System.out.println("Database connection established");
+        } catch (SQLException | IllegalStateException e) {
+            System.out.println("Database connection establishment failed: " + e.getMessage());
+        }
+    }
 }
